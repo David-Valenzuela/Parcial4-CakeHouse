96a4ffaf561131506ee0d2b7b40595928724d129
nicolas prueba <nicolas@prueba.com>
2021-07-02 16:10:57 -0400

    Modificacion en datos de Contraseña

--- pasteleria/urls.py
+++ pasteleria/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.menu, name='menu'),
     path('agregar_cliente', views.agregar_cliente, name='agregar_cliente'),
     path('registrar_datos', views.registrar_datos, name='registrar_datos'),
-    path('registrocontraseña', views.registrocontraseña, name='registrocontraseña'),
+    path('registro_clave', views.registro_clave, name='registro_clave'),
 
     path('iniciar_sesion', views.iniciar_sesion, name='iniciar_sesion'),
     path('autenticar_usuario', views.autenticar_usuario, name='autenticar_usuario'),
