b8dda5cbacdbc0436ab7427a2db50f1dc9ea5dfc
Antonia Ruz <ant.ruz@duocuc.cl>
2021-07-02 21:47:58 -0400

    Se agrega archivo js y css de producto.html

---
+++ pasteleria/static/pasteleria/js/producto.js
@@ -0,0 +1,37 @@
+function unidad() {
+    var und = document.forms["pastel"]["und"].value;
+
+    if (und < 1 || und > 5) {
+        document.getElementById("alerta").style.visibility = "visible";
+        document.getElementById("und").style.border = "solid 2px red";
+        return false;
+    } else {
+        document.getElementById("alerta").style.visibility = "hidden";
+        document.getElementById("und").style.border = "";
+    };
+}
+
+$(document).ready(function () {
+    /* Registro*/
+    $("#registro").on("mouseover", function () {
+        $(this).css({
+            "box-shadow":"0 0 30px rgb(0,0,0,0.568)"
+        });
+    });
+    $("#registro").on("mouseleave", function () {
+        $(this).css({
+            "box-shadow": ""
+        });
+    });
+    /*Producto*/
+    $("#producto").on("mouseover", function () {
+        $(this).css({
+        "box-shadow": "0 0 30px rgb(0,0,0,0.568)"
+        });
+    });
+    $("#producto").on("mouseleave", function () {
+        $(this).css({
+        "box-shadow": ""
+        });
+    });
+});
