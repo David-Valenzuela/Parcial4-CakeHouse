e47749b40d61529a6e77c5b4479b80ec210eb0b7
David Valenzuela <dav.valenzuelat@duocuc.cl>
2021-07-03 19:29:31 -0400

    Modificacion en views y urls para que se ingrese directamente al login

--- pasteleria/urls.py
+++ pasteleria/urls.py
@@ -3,14 +3,16 @@ from django.urls import path
 from . import views
 app_name = 'pasteleria'
 urlpatterns = [
-    path('', views.menu, name='menu'),
+    
+    path('', views.iniciar_sesion, name='iniciar_sesion'),
+    path('autenticar_usuario', views.autenticar_usuario, name='autenticar_usuario'),
+    path('cerrar_sesion', views.cerrar_sesion, name='cerrar_sesion'),
+
     path('agregar_cliente', views.agregar_cliente, name='agregar_cliente'),
     path('registrar_datos', views.registrar_datos, name='registrar_datos'),
     path('registro_clave', views.registro_clave, name='registro_clave'),
 
-    path('iniciar_sesion', views.iniciar_sesion, name='iniciar_sesion'),
-    path('autenticar_usuario', views.autenticar_usuario, name='autenticar_usuario'),
-    path('cerrar_sesion', views.cerrar_sesion, name='cerrar_sesion'),
+    path('menu', views.menu, name='menu'),
     path('<int:producto_id>', views.producto, name='producto'),
     path('<int:producto_id>/pago/', views.pago, name='pago'),
 ]
